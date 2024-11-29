b0e8391bfcec67fe3b5979624a761a62f61dde3a
Aditya Raina <adityaraina788@gmail.com>
2024-11-30 00:17:07 +0530

    Add files via upload

--- LEETCODE SOLS/normaltriangle.java
+++ LEETCODE SOLS/normaltriangle.java
@@ -12,7 +12,7 @@ public class normaltriangle {
             }
 
             //star
-            for( int j=0; j<2*i-1; j++){
+            for( int j=0; j<2*i+1; j++){
                 System.out.print("*");
 
             }
